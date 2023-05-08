cf53f49c844ca0fabe8d88717e38c75233bb9a3e
lijiashu <lijiashu@joy-mask.com>
2023-05-08 12:18:26 +0800

    fix: 修复RedisConf的db字段取值

--- core/stores/redis/conf.go
+++ core/stores/redis/conf.go
@@ -22,7 +22,7 @@ type (
 		Pass     string `json:",optional"`
 		Tls      bool   `json:",optional"`
 		NonBlock bool   `json:",default=true"`
-		DB       int    `json:"db,default=0"`
+		DB       int    `json:",default=0"`
 		// PingTimeout is the timeout for ping redis.
 		PingTimeout time.Duration `json:",default=1s"`
 	}
